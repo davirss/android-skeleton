66a60cce0c9acb4b78a58f43fe424a16b28c6509
Davi Ribeiro <daviribeirosoares@gmail.com>
2021-03-18 21:59:12 +0100

    Adding koin

--- app/src/main/java/br/com/drss/pokedex/App.kt
+++ app/src/main/java/br/com/drss/pokedex/App.kt
@@ -1,7 +1,21 @@
 package br.com.drss.pokedex
 
 import android.app.Application
+import br.com.drss.pokedex.home.sl.applicationModule
+import br.com.drss.pokedex.home.sl.networkModule
+import org.koin.core.context.startKoin
 
 class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin()
+    }
+
+    private fun startKoin() {
+        startKoin {
+            modules(applicationModule, networkModule)
+        }
+    }
 
 }
---
+++ app/src/main/java/br/com/drss/pokedex/home/sl/module.kt
@@ -0,0 +1,18 @@
+package br.com.drss.pokedex.home.sl
+
+import org.koin.dsl.module
+
+
+val repositoryModule = module {
+    single {
+
+    }
+}
+
+val networkModule = module {
+
+}
+
+val applicationModule = module {
+
+}
