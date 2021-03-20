c12927b31a035fe50baddb2b3d849dc9fa5ba93b
Davi Ribeiro <daviribeirosoares@gmail.com>
2021-03-20 14:37:42 +0100

    Creating list fragment layout, adding lifecycle ktx dependencies.

--- app/src/main/java/br/com/drss/pokedex/home/ui/PokemonListFragment.kt
+++ app/src/main/java/br/com/drss/pokedex/home/ui/PokemonListFragment.kt
@@ -1,9 +1,36 @@
 package br.com.drss.pokedex.home.ui
 
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import br.com.drss.pokedex.R
+import kotlinx.coroutines.flow.collect
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class PokemonListFragment: Fragment() {
+class PokemonListFragment: Fragment(R.layout.fragment_pokemon_list) {
 
+    private val pokemonListViewModel: PokemonListViewModel by viewModel()
 
+    init {
+
+        lifecycleScope.launchWhenResumed {
+            pokemonListViewModel.viewState.collect {
+                renderUi(it)
+            }
+        }
+    }
+
+    private fun renderUi(viewState: PokemonListViewState) {
+        when (viewState) {
+            is Loaded -> displayLoadedState(viewState)
+            Loading -> displayLoading(viewState)
+        }
+    }
+
+    private fun displayLoading(viewState: PokemonListViewState) {
+    }
+
+    private fun displayLoadedState(viewState: Loaded) {
+
+    }
 
 }
