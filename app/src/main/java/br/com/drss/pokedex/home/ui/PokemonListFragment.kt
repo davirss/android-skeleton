7d72ccf0bf0934d3240a99ceefd9c35cbcd94ce3
Davi Ribeiro <daviribeirosoares@gmail.com>
2021-03-20 14:44:08 +0100

    Adding view binding to the project

--- app/src/main/java/br/com/drss/pokedex/home/ui/PokemonListFragment.kt
+++ app/src/main/java/br/com/drss/pokedex/home/ui/PokemonListFragment.kt
@@ -1,17 +1,21 @@
 package br.com.drss.pokedex.home.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
-import br.com.drss.pokedex.R
+import br.com.drss.pokedex.databinding.FragmentPokemonListBinding
 import kotlinx.coroutines.flow.collect
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class PokemonListFragment: Fragment(R.layout.fragment_pokemon_list) {
+class PokemonListFragment: Fragment() {
 
     private val pokemonListViewModel: PokemonListViewModel by viewModel()
+    private lateinit var binding: FragmentPokemonListBinding
 
     init {
-
         lifecycleScope.launchWhenResumed {
             pokemonListViewModel.viewState.collect {
                 renderUi(it)
@@ -19,14 +23,24 @@ class PokemonListFragment: Fragment(R.layout.fragment_pokemon_list) {
         }
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentPokemonListBinding.inflate(inflater)
+        return binding.root
+    }
+
     private fun renderUi(viewState: PokemonListViewState) {
         when (viewState) {
             is Loaded -> displayLoadedState(viewState)
-            Loading -> displayLoading(viewState)
+            Loading -> displayLoading()
         }
     }
 
-    private fun displayLoading(viewState: PokemonListViewState) {
+    private fun displayLoading() {
+
     }
 
     private fun displayLoadedState(viewState: Loaded) {
